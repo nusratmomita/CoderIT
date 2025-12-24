d62906e3443b7240446c1cce62d003b207f61e65
nusratmomita <108608844+nusratmomita@users.noreply.github.com>
2025-12-24 17:41:07 +0600

    Designed till about us section

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// for banner slider
+var swiperBanner = new Swiper(".hero_banner_slider", {
+  loop: true,
+  pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+});
