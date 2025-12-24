986271db640b7fcb441dcee79bc80a416857f761
nusratmomita <108608844+nusratmomita@users.noreply.github.com>
2025-12-25 00:55:08 +0600

    Success number section done

--- js/script.js
+++ js/script.js
@@ -7,3 +7,39 @@ var swiperBanner = new Swiper(".hero_banner_slider", {
         dynamicBullets: true
     },
 });
+
+function startCounter() {
+    $('.counter').each(function () {
+        const $this = $(this);
+        const target = +$this.data('target');
+
+        $({ countNum: 0 }).animate(
+            { countNum: target },
+            {
+                duration: 2000,
+                easing: 'swing',
+                step: function () {
+                    $this.text(Math.floor(this.countNum) + '+');
+                },
+                complete: function () {
+                    $this.text(target + '+');
+                }
+            }
+        );
+    });
+}
+
+let counterStarted = false;
+
+const observer = new IntersectionObserver(
+    function (entries) {
+        if (entries[0].isIntersecting && !counterStarted) {
+            startCounter();
+            counterStarted = true;
+            observer.disconnect(); // run only once
+        }
+    },
+    { threshold: 0.4 } // 40% visible
+);
+
+observer.observe(document.querySelector('#stats'));
