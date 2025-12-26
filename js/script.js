c0a6a55d100ee3e09bbee09c933a691c06fa79cc
nusratmomita <108608844+nusratmomita@users.noreply.github.com>
2025-12-26 11:21:40 +0600

    Redesigned banner

--- js/script.js
+++ js/script.js
@@ -66,5 +66,45 @@ function typeWriter() {
   }
 }
 
-
 typeWriter();
+
+
+const text2 = "Workflow.";
+const speed2 = 120;     // typing speed
+const delay2 = 1200;    // pause after full word
+let index2 = 0;
+
+function typeWriter2() {
+  const el = document.getElementById("typewriter2");
+
+  if (index2 < text2.length) {
+    el.textContent += text2.charAt(index2);
+    index2++;
+    setTimeout(typeWriter2, speed2);
+  } else {
+    setTimeout(() => {
+      el.textContent = "";
+      index2 = 0;
+      typeWriter2();
+    }, delay2);
+  }
+}
+
+typeWriter2();
+
+var swiper = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
